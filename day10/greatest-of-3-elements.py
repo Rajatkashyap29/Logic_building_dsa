055dc7b5ddc16dc91531cec96d74575e777dc076
Rajatkashyap29 <mr.rajat.29@gmail.com>
2026-04-19 22:02:22 +0530

    todays update

---
+++ day10/greatest-of-3-elements.py
@@ -0,0 +1,11 @@
+A=int(input("ENTER FIRST NUMBER:"))
+B=int(input("ENTER SECOND NUMBER:"))
+C=int(input("ENTER THIRD NUMBER:"))
+
+if A > B and A>C :
+    print("greatest number is -:",A)
+elif B>A and B>C:
+    print("greatest number is -:",B) 
+else:
+    print("greatest number is -:",C)
+           

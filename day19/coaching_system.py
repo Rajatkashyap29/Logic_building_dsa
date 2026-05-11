68379fbd2321f81fcdb1d1f8620e36b7f1f88f22
Rajatkashyap29 <rajat.29.singh.11@gmail.com>
2026-05-11 22:30:20 +0530

    day 19 update

---
+++ day19/coaching_system.py
@@ -0,0 +1,10 @@
+class Coaching:
+    def __init__(self,name,age,cource,marks):
+        self.name = name 
+        self.age = age 
+        self.cource = cource
+        self.marks = marks 
+        
+    
+    
+        

db8156ffca2dc74232df50468d8cf8157ff3c2c2
Rajatkashyap29 <rajat.29.singh.11@gmail.com>
2026-05-12 21:34:07 +0530

    coaching system update

--- day19/coaching_system.py
+++ day19/coaching_system.py
@@ -1,9 +1,35 @@
 class Coaching:
+    Coaching_name = "Bright Future Academy "
+    total_student = 0
+    
     def __init__(self,name,age,cource,marks):
         self.name = name 
         self.age = age 
         self.cource = cource
         self.marks = marks 
+        
+        
+        Coaching.total_student +=1
+        
+    def show_res(self):
+            if self.marks >=30:
+                print(f"Final result of {self.name} is PASS ")
+            else:
+             (f"Final result of {self.name} is Fail ")
+
+
+print("Coaching Name:", Coaching.coaching_name)
+print("Total Students:", Coaching.total_students)
+
+s1 = Coaching("rajat",20,"AI",89)             
+s2 = Coaching("rishav",28,"ML",21)             
+s3 = Coaching("ritik",24,"block-chain",23)             
+
+
+s1.show_res()   
+s2.show_res()         
+                    
+        
         
     
     
